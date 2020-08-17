ae6b7a5719903e5c563e52f101ae7c8875687b6a
songyixian <kangxizhizi@163.com>
2020-08-17 13:52:01 +0800

    update

--- a_caculate/2020-06-10-RomanToInt.py
+++ a_caculate/2020-06-10-RomanToInt.py
@@ -1,4 +1,9 @@
+d = {'I': 1, 'IV': 3, 'V': 5, 'IX': 8, 'X': 10, 'XL': 30, 'L': 50, 'XC': 80, 'C': 100, 'CD': 300, 'D': 500,
+     'CM': 800, 'M': 1000}
+
+
 class Solution(object):
+
     def romanToInt1(self, s):
         """
         :type s: str
@@ -13,6 +18,7 @@ class Solution(object):
             "D": 500,
             "M": 1000
         }
+
         print(s[0:1])
         fake_result = 0
         for element in s:
@@ -26,11 +32,21 @@ class Solution(object):
         return fake_result
 
     def romanToInt2(self, s):
-        d = {'I': 1, 'IV': 3, 'V': 5, 'IX': 8, 'X': 10, 'XL': 30, 'L': 50, 'XC': 80, 'C': 100, 'CD': 300, 'D': 500,
-             'CM': 800, 'M': 1000}
+
         return sum(d.get(s[max(i - 1, 0):i + 1], d[n]) for i, n in enumerate(s))
 
+    def romanToInt3(self, s):
+        result = 0
+        for i,n in enumerate(s):
+            a = s[max(i-1,0):i+1]
+            b=d.get(a,d[n])
+            print(a)
+            print(b)
+            result += b
+        return result
 
 a = Solution()
-# a.romanToInt("MMCMLXXXIV")
-print(a.romanToInt1("III"))
+# # a.romanToInt("MMCMLXXXIV")
+# print(a.romanToInt1("III"))
+# a = Solution
+print(a.romanToInt3("MMCMLXXXIV"))
