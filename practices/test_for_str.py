f43fd3e1e5d77597b919d6a86a3c2382e7122b4c
songyixian <kangxizhizi@163.com>
2020-10-09 11:59:12 +0800

    update

--- practices/test_for_str.py
+++ practices/test_for_str.py
@@ -1,8 +1,24 @@
 # i = []
 # s = "\n".join(i)
-a = {}
-d = [i for i in a.values()]
-print("-----{}----".format(d))
+# a = {}
+# d = [i for i in a.values()]
+# print("-----{}----".format(d))
 # print(a.values())
 # print(len(s))
 # print("------{}--------".format(s))
+import asyncio
+import json
+
+import httpx as httpx
+
+
+async def run_httpx():
+    async with httpx.AsyncClient() as client:
+        r = await client.get('http://www.baidu.com')
+        assert r.status_code == 200
+        a = json.loads(r.text)
+        print(a)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_httpx())
